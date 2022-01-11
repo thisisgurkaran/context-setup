8f243c1902f1ce14b81bef4d6109c869658e492d
gurkaran singh <gurkaran.12@gmail.com>
2022-01-11 02:40:24 -0500

    Comments added

--- src/components/ChildForm.js
+++ src/components/ChildForm.js
@@ -1,8 +1,9 @@
 import React from "react";
 import useFormContext from "../contexts/useFormContext";
 const ChildForm = () => {
+  //Get the state and render the state.name
   const { state } = useFormContext();
-  return <div>{state.name}</div>;
+  return <div>My Name is: {state.name}</div>;
 };
 
 export default ChildForm;
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,10 @@ import React from "react";
 import useFormContext from "../contexts/useFormContext";
 import ChildForm from "./ChildForm";
 const Form = () => {
+  //get the state and dispatch function from useFormContext. Update the state with the dispatch function
   const { state, dispatch } = useFormContext();
   console.log(state);
+  //input will dispatch the value onChange
   return (
     <div>
       <input
@@ -11,6 +13,7 @@ const Form = () => {
           dispatch({ type: "setName", payload: { name: e.target.value } })
         }
       />
+      <p>Child Component Child Form will have access to global context</p>
       <ChildForm />
     </div>
   );
